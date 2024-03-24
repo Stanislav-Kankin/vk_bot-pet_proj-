5f0d0208f61bc1434ba90f0e5fc2c1069ec9103d
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-03-24 16:48:08 +0300

    добавил простую игру угадай число

--- bot_vk.py
+++ bot_vk.py
@@ -1,19 +1,52 @@
 from vk_api import VkApi, VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
+from random import randint
 
 from config import BOT_API_KEY
 
 
-def write_message(sender, message):
+def write_message(sender, message, attachments=None):
     authorize.method(
         'messages.send', {
             'user_id': sender,
             'message': message,
             'random_id': get_random_id(),
-            'attachments': ','.join(attachments)
+            'attachment': ','.join(attachments) if attachments else None
         }
     )
+
+
+def play_guess_number(sender):
+    secret_number = randint(1, 25)
+    write_message(sender, 'Я загадал число от 1 до 25. Попробуй угадать!')
+    write_message(sender, 'У тебя 5 попыток)')
+
+    try_count = 0
+    while try_count < 5:
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                user_message = event.text
+                try:
+                    guess = int(user_message)
+                    if guess == secret_number:
+                        write_message(sender, 'Поздравляю, ты угадал число!')
+                        return
+                    elif guess < secret_number:
+                        write_message(sender, 'Загаданное число больше!')
+                    else:
+                        write_message(sender, 'Загаданное число меньше!')
+                    try_count += 1
+                    break
+                except ValueError:
+                    write_message(sender, 'Некорректный ввод. Попробуй еще раз.')
+
+    write_message(sender, 'К сожалению, ты не угадал число. Загаданное число было: {}'.format(secret_number))
+
+    write_message(
+        sender,
+        'К сожалению, ты не угадал число. Загаданное число было: {}'.format(secret_number)
+        )
 
 
 authorize = VkApi(token=BOT_API_KEY)
@@ -33,11 +66,14 @@ try:
             attachments.append(
                 'photo{}_{}'.format(
                     upload_image['owner_id'], upload_image['id']
-                    ))
+                ))
             if user_message == 'Привет':
-                write_message(sender, 'Здарова!!!')
+                write_message(sender, 'Здарова!!!', attachments)
+                write_message(sender, 'Чтобы начать игру нпиши "Играем"')
             elif user_message == 'Пока':
                 write_message(sender, 'Досвидули!')
+            elif user_message == 'Играем':
+                play_guess_number(sender)
             else:
                 write_message(sender, 'Я не понимаю эту строку!!!')
 except KeyboardInterrupt:
